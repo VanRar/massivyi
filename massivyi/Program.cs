f5a2d801119bfe4fae566831209300c54e4ea576
VanRar <vitalikf7@gmail.com>
2020-06-27 18:52:11 +0300

    Добавьте файлы проекта.

---
+++ massivyi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace massivyi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
